9c3038909ad5d35a9148bfcf7e1e3dc2f8548b9e
RAMSES <95148596+RAMSES-DG@users.noreply.github.com>
2023-06-14 10:25:24 +0000

    Add files via upload

---
+++ data/helps.py
@@ -0,0 +1,103 @@
+main_help="""
+
+@ help list -
+            - you can run any command here
+
+  command             | service
+----------------------------------------
+| set [ option text ] | to choosen tool control
+| help                | to show available commands
+| exit                | to close my tool
+| clear               | to clear the screen
+| options             | to show available options
+"""
+
+tools_help="""
+
+@ tool list -
+            - you can run any command here on the choosen tool
+
+  command             | service
+------------------------------------------
+| help                | to show available commands
+| back                | to undo the last command
+| clear               | to clear the screen
+| exit                | to close my tool
+| open                | to open any tool
+
+"""
+dlinks_help="""
+
+@ darkweb links list -
+                     - you can run any command here
+
+  command             | service
+------------------------------------------
+| help                | to show available commands
+| back                | to undo the last command
+| clear               | to clear the screen
+| exit                | to close my tool
+| show                | to show available links
+
+"""
+
+main_options="""
+
+@ options list -
+               - you can run any command here by ( set + " option " )
+
+  option              | service
+------------------------------------------
+| dlinks              | darkweb choosen links
+| tools               | linux/termux tools
+| books               | choosen hidden books
+
+"""
+opening_help="""
+
+@ open tools help -
+                  - you can run any command here
+                  - you can run ( xx ) code for run any tool
+
+  option             | service
+------------------------------------------
+| code : ( xx )      | to open the tool of code
+| help               | to show available commands
+| clear              | to clear the screen
+| exit               | to exit my tool
+| back               | to undo last command
+| show               | to show available tools
+
+"""
+books_help="""
+
+@ books help -
+                  - you can run any command here
+
+
+  option             | service
+------------------------------------------
+| help               | to show available commands
+| clear              | to clear the screen
+| exit               | to exit my tool
+| back               | to undo last command
+| open               | to open choosen book
+
+"""
+openingb_help="""
+
+@ open books help -
+                  - you can run any command here
+                  - you can run ( xx ) code for open any book
+
+  option             | service
+------------------------------------------
+| code : ( xx )      | to open the book of code
+| help               | to show available commands
+| clear              | to clear the screen
+| exit               | to exit my tool
+| back               | to undo last command
+| show               | to show available books
+| cinfo              | to show more infos about codes
+
+"""
